4682f4a7842315722293c5676f617ab99646ea46
Bradley Cai <bradcai24@gmail.com>
2016-07-28 17:41:43 -0700

    Reorganized js, swapped out svg image arrow with octicon arrow, styled the button, and added fallback functionality to the button

--- js/scroller.js
+++ js/scroller.js
@@ -1,5 +1,12 @@
-$("#browse-videos-button").click(function() {
-    $('html,body').animate({
-        scrollTop: $("#video-list").offset().top},
-        500);
-});
+var initScrollerButton = function() {
+    // Removes the fallback functionality
+    $("#browse-videos-button").replaceWith('<a class="btn" id="browse-videos-button">Browse Videos<br><span class="octicon octicon-chevron-down"></span></a>');
+
+    // Gives the "Browse Videos" button the ability to scroll to the videos section div
+    $("#browse-videos-button").click(function() {
+        $('html,body').animate(
+            {scrollTop: $("#video-list").offset().top},
+            500
+        );
+    });
+};
